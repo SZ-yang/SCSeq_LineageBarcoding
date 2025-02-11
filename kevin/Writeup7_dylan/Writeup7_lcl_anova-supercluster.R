08eeaf702983d9d2ba4c17e30217d616b3cb3cb8
linnykos <kzlin@uw.edu>
2025-02-11 08:42:44 -0800

    About to try more plotting

--- kevin/Writeup7_dylan/Writeup7_lcl_anova-supercluster.R
+++ kevin/Writeup7_dylan/Writeup7_lcl_anova-supercluster.R
@@ -11,6 +11,20 @@ source("kevin/Writeup7_dylan/welch_anova.R")
 
 load(paste0(out_folder, "adata_with_lcl.RData"))
 seurat_obj$OG_condition <- factor(seurat_obj$OG_condition)
+seurat_obj$Lineage <- factor(seurat_obj$Lineage)
+
+color_vec <- c("cis" = rgb(202,111,55, maxColorValue = 255),
+               "cistocis" = rgb(248,210,152, maxColorValue = 255),
+               "cistococl2" = rgb(240,148,71, maxColorValue = 255),
+               "cistodabtram" = rgb(160,61,38, maxColorValue = 255),
+               "cocl2" = rgb(69,132,69, maxColorValue = 255),
+               "cocl2tocis" = rgb(131,202,163, maxColorValue = 255),
+               "cocl2tococl2" = rgb(126,191,90, maxColorValue = 255),
+               "cocl2todabtram" = rgb(35,63,58, maxColorValue = 255),
+               "dabtram" = rgb(68,49,147, maxColorValue = 255),
+               "dabtramtocis" = rgb(147,137,193, maxColorValue = 255),
+               "dabtramtococl2" = rgb(145,54,147, maxColorValue = 255),
+               "dabtramtodabtram" = rgb(68,32,85, maxColorValue = 255))
 
 embedding <- seurat_obj[["lcl"]]@cell.embeddings
 apply(embedding, 2, quantile)
@@ -41,9 +55,12 @@ cluster_names <- colnames(tab_mat)[which(cluster_sizes > 200)]
 keep_vec <- (seurat_obj$RNA_snn_res.0.01 %in% cluster_names)
 seurat_obj$keep <- keep_vec
 seurat_obj <- subset(seurat_obj, keep == TRUE)
-Seurat::DimPlot(seurat_obj,
+plot1 <- Seurat::DimPlot(seurat_obj,
                 group.by = "RNA_snn_res.0.01",
                 reduction = "lcl.umap")
+ggplot2::ggsave(plot1,
+                filename = paste0(plot_folder, "Writeup7_lcl_anova-refinement1.png"),
+                height = 5, width = 8)
 
 embedding <- seurat_obj[["lcl"]]@cell.embeddings
 dbscan_res <- dbscan::dbscan(
@@ -56,16 +73,15 @@ dbscan_cluster[dbscan_cluster == 0] <- NA
 names(dbscan_cluster) <- Seurat::Cells(seurat_obj)
 seurat_obj$dbscan <- dbscan_cluster
 
-Seurat::DimPlot(seurat_obj,
+plot1 <- Seurat::DimPlot(seurat_obj,
                 group.by = "dbscan",
                 reduction = "lcl.umap")
+ggplot2::ggsave(plot1,
+                filename = paste0(plot_folder, "Writeup7_lcl_anova-refinement2.png"),
+                height = 5, width = 8)
 
 seurat_obj$keep <- !is.na(seurat_obj$dbscan)
 seurat_obj <- subset(seurat_obj, keep == TRUE)
-
-Seurat::DimPlot(seurat_obj,
-                group.by = "RNA_snn_res.0.01",
-                reduction = "lcl.umap")
 
 seurat_obj <- Seurat::FindClusters(seurat_obj, 
                                    resolution = 0.01)
@@ -74,17 +90,31 @@ cluster_names <- names(tab_vec)[which(tab_vec > 200)]
 keep_vec <- (seurat_obj$RNA_snn_res.0.01 %in% cluster_names)
 seurat_obj$keep <- keep_vec
 seurat_obj <- subset(seurat_obj, keep == TRUE)
-Seurat::DimPlot(seurat_obj,
+plot1 <- Seurat::DimPlot(seurat_obj,
                 group.by = "RNA_snn_res.0.01",
                 reduction = "lcl.umap")
+ggplot2::ggsave(plot1,
+                filename = paste0(plot_folder, "Writeup7_lcl_anova-refinement3.png"),
+                height = 5, width = 8)
 
 seurat_obj$Lineage <- factor(paste0("Lineage:", seurat_obj$Lineage))
 seurat_obj$Lineage <- droplevels(seurat_obj$Lineage)
 seurat_obj$RNA_snn_res.0.01 <- factor(paste0("Cluster:", seurat_obj$RNA_snn_res.0.01))
 
-scCustomize::DimPlot_scCustom(seurat_obj,
+plot1 <- scCustomize::DimPlot_scCustom(seurat_obj,
                               group.by = "Lineage",
                               reduction = "lcl.umap") + Seurat::NoLegend()
+ggplot2::ggsave(plot1,
+                filename = paste0(plot_folder, "Writeup7_lcl_anova-refinement3_by-lineage.png"),
+                height = 5, width = 8)
+
+plot1 <- scCustomize::DimPlot_scCustom(seurat_obj,
+                                       group.by = "OG_condition",
+                                       reduction = "lcl.umap",
+                                       colors_use = color_vec) + Seurat::NoLegend()
+ggplot2::ggsave(plot1,
+                filename = paste0(plot_folder, "Writeup7_lcl_anova-refinement3_by-OG_condition.png"),
+                height = 5, width = 8)
 
 ###########################
 
@@ -124,21 +154,25 @@ if(length(rm_idx) > 0){
   gene_mat <- gene_mat[-rm_idx,]
 }
 
-
 hallmark_csv <- readxl::read_excel(paste0(out_folder, "41586_2023_6130_MOESM6_ESM.xlsx"),
                                    sheet = "Cancer MPs")
 hallmark_csv <- as.data.frame(hallmark_csv)
+
+pdf(paste0(plot_folder, "Writeup7_lcl_anova_hallmark.pdf"),
+    height = 5, width = 8, onefile = TRUE)
 for(j in 1:ncol(hallmark_csv)){
   vec <- hallmark_csv[,j]
   vec <- intersect(vec, rownames(gene_mat))
   hist(gene_mat[,2],
        main = paste0("Column ", j, " (", colnames(hallmark_csv)[j], "),",
-                     "\n# genes: ", length(vec)))
+                     "\n# genes: ", length(vec)),
+       xlab = "Percent explained by LCL cluster (per gene)")
   idx <- which(rownames(gene_mat) %in% vec)
   rug(gene_mat[idx,2], 
       col = 2,
       lwd = 2)
 }
+graphics.off()
 
 ############################
 
@@ -176,13 +210,19 @@ seurat_obj <- Seurat::AddModuleScore(
 
 seurat_obj$Actin2 <- pmax(seurat_obj$Actin1, 0)
 
-scCustomize::FeaturePlot_scCustom(seurat_obj,
+plot1 <- scCustomize::FeaturePlot_scCustom(seurat_obj,
                                   features = "Actin2",
                                   reduction = "lcl.umap")
+ggplot2::ggsave(plot1,
+                filename = paste0(plot_folder, "Writeup7_lcl_anova_Actin_by-lcl.png"),
+                height = 5, width = 8)
 
-scCustomize::FeaturePlot_scCustom(seurat_obj,
+plot1 <- scCustomize::FeaturePlot_scCustom(seurat_obj,
                                   features = "Actin2",
                                   reduction = "umap")
+ggplot2::ggsave(plot1,
+                filename = paste0(plot_folder, "Writeup7_lcl_anova_Actin_by-umap.png"),
+                height = 5, width = 8)
 
 ##########
 
